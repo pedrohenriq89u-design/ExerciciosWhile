ea3c950f1b865c32f6bf40530a9d011c3f4a0863
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-04-15 13:47:09 -0300

    first commit

---
+++ src/Exercicio01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+        static void main() {
+
+            Scanner sc = new Scanner(System.in);
+
+            int numero = 10;
+            while (numero >= 0) {
+                IO.println("" + numero);
+                numero--;
+            }
+        }
+    }
+
