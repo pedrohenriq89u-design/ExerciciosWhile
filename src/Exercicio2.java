ebc2870608ea57e62e72b9f199f4af54307628ba
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-04-15 13:51:46 -0300

    feat(ex02):contador.

---
+++ src/Exercicio2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+    static void main(){
+
+        Scanner sc = new Scanner(System.in);
+
+        IO.println("Digite um numero: ");
+        int numerousuario = sc.nextInt();
+        int resultado = 0;
+        int contador = 1;
+
+        while (contador <= numerousuario){
+            IO.println(resultado);
+            IO.println(contador);
+            resultado = resultado + contador;
+            contador++;
+
+        }
+        IO.println(resultado);
+    }
+}
+
