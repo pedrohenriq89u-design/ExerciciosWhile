db073a05fcb192c8d080e55ceaf0a00d58d85746
pedrohenriq89u-design <pedrohenriq89u@gmail.com>
2026-04-15 16:37:52 -0300

    Update Exercicio09.java

--- src/Exercicio09.java
+++ src/Exercicio09.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Exercicio09 {
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        double i, num = 0;
+
+
+        IO.println("Digite um numero: (0 para parar):");
+        i = sc.nextDouble();
+
+        while (i != 0) {
+            num = i;
+            i = sc.nextDouble();
+            if ( num < i ){
+                num = i;
+            }
+
+        }
+        IO.println("Seu maior numero: " + num);
+    }
 }
